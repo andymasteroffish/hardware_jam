92d6995aa9326c3fc238273099ba21b7cc51e6c1
Andy Wallace <andymasteroffish@gmail.com>
2018-11-17 16:05:35 -0500

    simple test button

--- hardware_jam/hardware_jam.ino
+++ hardware_jam/hardware_jam.ino
@@ -1,5 +1,11 @@
 #include <Adafruit_NeoPixel.h>
 
+struct Button{
+  int pin;
+  int next_check_time;  //for debounce
+  bool is_on;
+};
+
 #define NEO_PIXEL_START_PIN 6
 
 #define NUM_COLS 5
@@ -12,11 +18,17 @@ Adafruit_NeoPixel pixel_rows[NUM_ROWS];
 Adafruit_NeoPixel pixel_rows0 = Adafruit_NeoPixel(NUM_COLS, NEO_PIXEL_START_PIN, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel pixel_rows1 = Adafruit_NeoPixel(NUM_COLS, NEO_PIXEL_START_PIN+1, NEO_GRB + NEO_KHZ800);
 
+//buttons
+int debounce_time = 50; //millis
+Button test_button;
 
 
 //test states
 int player_x = 0;
 int player_y = 0;
+
+
+
 
 void setup() {
   Serial.begin(9600); 
@@ -29,6 +41,11 @@ void setup() {
     pixel_rows[i].begin();
   }
 
+  //buttons
+  test_button.pin = 2;
+  test_button.next_check_time = 0;
+  test_button.is_on = false;
+  pinMode(test_button.pin, INPUT_PULLUP);
 
   Serial.println("I am ready to go.");
 
@@ -37,7 +54,7 @@ void setup() {
 void loop() {
 
 
-  //checking for input
+  //checking for serial test input
   if (Serial.available() > 0) {
     String incoming = Serial.readString();
     Serial.println(incoming);
@@ -62,9 +79,36 @@ void loop() {
   
   }
 
+  //checking button input
+  check_input();
+
   //showing the thing
   display_game();
 
+}
+
+void check_input(){
+
+  //this should be an array of buttons
+  if (millis() > test_button.next_check_time){
+    //get the current value
+    bool cur_val = false;
+    if (digitalRead(test_button.pin) == LOW){
+      cur_val = true;
+    }
+
+    //is that different form the current value?
+    if (cur_val != test_button.is_on){
+      test_button.is_on = cur_val;
+      test_button.next_check_time = millis() + debounce_time;
+      Serial.println("button: "+String(test_button.is_on));
+      if (test_button.is_on){
+        player_x++;
+        if (player_x >= NUM_COLS) player_x = 0;
+      }
+    }
+    
+  }
 }
 
 void display_game(){
